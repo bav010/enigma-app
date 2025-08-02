c9262f4517aceeeecc6b7384af1e5f8a6754c548
bav010 <150358688+bav010@users.noreply.github.com>
2025-08-02 21:31:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("enigma-v1").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
